a49698c7c8362146f6416dadff50459d994be845
Alithebest-coder <aurumprint@proton.me>
2024-11-18 17:39:08 +1000

    Add files via upload

---
+++ division.py
@@ -0,0 +1,12 @@
+a=13
+b=46
+c=a/b
+ 
+e=int(input("enter e number"))
+f=int(input("enter f number"))
+
+print()
+print(e/f)
+print("c")
+print(a/b)
+print()
---
+++ operator.py
@@ -0,0 +1,12 @@
+a=100
+
+b=12
+c=78
+h=70
+k=54
+g=c*(a+h)
+z=k*(c-g)
+print()
+print(g)
+print(z)
+print()
